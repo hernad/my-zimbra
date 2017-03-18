72c005fcaf8a9e83be9543ca876a5fd7a07d565b
hernad <hernad@bring.out.ba>
2017-03-18 17:35:45 +0100

    my-zimbra is GNU software

--- my-patches.sh
+++ my-patches.sh
@@ -7,3 +7,14 @@ sed -i -e 's/postinstall::notifyZimbra\(\).*$/print "no zimbra notify\\\n";/' Zi
 echo "no license question"
 sed -i -e 's/displayLicense/print "no license question\\\n";/'  ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
 
+
+splashScreenCompanyURL = https://www.zimbra.com
+
+MY_COMPANY_URL='https:\/\/github.com\/hernad\/my-zimbra'
+
+for f in ZimbraWebClient/WebRoot/messages/ZmMsg*.properties
+do
+   echo $f
+   sed -i -e "s/splashScreenCompanyURL.*$/splashScreenCompanyURL = $MY_COMPANY_URL/" $f
+done
+
