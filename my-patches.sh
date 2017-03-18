ee1bcf85676bca54798383984d01cc06070d577a
hernad <hernad@bring.out.ba>
2017-03-18 20:19:31 +0100

    installer cleanup

--- my-patches.sh
+++ my-patches.sh
@@ -5,7 +5,19 @@ echo "exclude notify from install procedure"
 sed -i -e 's/postinstall::notifyZimbra\(\).*$/print "no zimbra notify\\\n";/' ZimbraBuild/corebuild/opt/zimbra/libexec/zmsetup.pl 
 
 echo "no license question"
-sed -i -e 's/displayLicense/print "no license question\\\n";/'  ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
+
+sed -i -e 's/displayLicense$/echo "no license question"/'  ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
+sed -i -e 's/displayThirdPartyLicenses$/echo "no third party licenses question"/'  ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
+sed -i -e 's/configurePackageServer$/echo "no configurePackageServer - no setup zimbra deb repository"/'  ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
+
+sed -i -e 's/askYN.*Do you wish to upgrade.*Y.*$/response="yes"/' ZimbraBuild/corebuild/opt/zimbra/libexec/installer/util/utilfunc.sh
+
+FILE="ZimbraBuild/corebuild/opt/zimbra/libexec/installer/util/modules/packages.sh
+# $PACKAGEDOWNLOAD $repocomp >> $LOGFILE 2>&1
+sed -i -e 's/.*$PACKAGEDOWNLOAD $repocomp.*$/echo "no remote package download"/' $FILE
+
+# "$REPOINST $repocomp >>$LOGFILE 2>&1"
+sed -i -e 's/.*$REPOINST $repocomp.*$/echo "no remote package install"/' $FILE
 
 
 splashScreenCompanyURL = https://www.zimbra.com
