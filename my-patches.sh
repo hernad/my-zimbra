b20dbb6fa0027eb0c6d124ac1eaee66959ab64b3
hernad <hernad@bring.out.ba>
2017-03-19 17:13:10 +0100

    ZimbraBuild/rpmconf correct location

--- my-patches.sh
+++ my-patches.sh
@@ -3,24 +3,23 @@
 echo "exclude notify from install procedure"
 
 
-FILE=ZimbraBuild/corebuild/opt/zimbra/libexec/zmsetup.pl 
+FILE=ZimbraBuild/rpmconf/Install/zmsetup.pl
 sed -i -e 's/postinstall::notifyZimbra\(\).*$/print "no zimbra notify\\\n";/' $FILE
 
 echo "no license question"
 
-FILE=ZimbraBuild/corebuild/opt/zimbra/libexec/installer/install.sh
+FILE=ZimbraBuild/rpmconf/Install/install.sh
 sed -i -e 's/displayLicense$/echo "no license question"/' $FILE
 sed -i -e 's/displayThirdPartyLicenses$/echo "no third party licenses question"/'  $FILE
 sed -i -e 's/configurePackageServer$/echo "no configurePackageServer - no setup zimbra deb repository"/' $FILE
 
 
-FILE=ZimbraBuild/corebuild/opt/zimbra/libexec/installer/util/utilfunc.sh
+FILE=ZimbraBuild/rpmconf/Install/Util/utilfunc.sh
 sed -i -e 's/askYN.*Do you wish to upgrade.*Y.*$/response="yes"/' $FILE
 sed -i -e 's/verifyUpgrade$/echo "no verifyUpgrade"/' $FILE
 
 
-
-FILE=ZimbraBuild/corebuild/opt/zimbra/libexec/installer/util/modules/packages.sh
+FILE=ZimbraBuild/rpmconf/Install/Util/modules/ ZimbraBuild/packages.sh
 # $PACKAGEDOWNLOAD $repocomp ...
 sed -i -e 's/.*\$PACKAGEDOWNLOAD \$repocomp.*$/echo "no remote package download"/' $FILE
 
